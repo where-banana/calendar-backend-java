7e34b3c0e6bf946fe9cb15284c3bd28d0f6ceb10
Alexey Savchuk <sav.alex1917@gmail.com>
2023-01-13 17:13:36 -0500

    Add Cors configuration

--- calendar/src/main/java/pet/project/calendar/CalendarApplication.java
+++ calendar/src/main/java/pet/project/calendar/CalendarApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import pet.project.calendar.entity.*;
 import pet.project.calendar.repository.*;
 
@@ -19,65 +24,17 @@ public class CalendarApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalendarApplication.class, args);
 	}
-	@Bean
-	public CommandLineRunner test(WorkspaceRepository workspaceRepository,
-								  EventRepository eventRepository,
-								  NoteRepository noteRepository){
-		return args -> {
-			//TODO Test delete method for all Repos
-//---------------------------------Credentials------------------
 
-//			Optional<Credential> cred1 = credentialRepository.findCredentialsById(2);
-//			cred1.ifPresent(value -> log.info(value.toString()));
-//
-//			Optional<Credential> cred2 = credentialRepository.findCredentialsByEmail("arteic4@yandex.ru");
-//			cred2.ifPresent(value -> log.info(value.toString()));
-//
-//			Optional<Credential> cred3 = credentialRepository.findCredentialsByUserId(5);
-//			cred3.ifPresent(value -> log.info(value.toString()));
-//
-//			Iterable<Credential> cred4 = credentialRepository.findAll();
-//			List<Credential> cred4res = (List<Credential>) cred4;
-//			log.info(cred4res.toString());
+	// @Bean
+	// public WebMvcConfigurer corsConfigurer() {
 
-
-//-------------------------------------Workspaces------------
-//			Optional<Workspace> work1 = workspaceRepository.findWorkspaceById(3);
-//			work1.ifPresent(value -> log.info(value.toString()));
-//
-//			Iterable<Workspace> work2 = workspaceRepository.findWorkspacesByName("Study");
-//			List<Workspace> work2res = (List<Workspace>) work2;
-//			log.info(work2res.toString());
-//
-//			Iterable<Workspace> work3 = workspaceRepository.findWorkspacesByUserId(7);
-//			List<Workspace> work3res = (List<Workspace>) work3;
-//			log.info(work3res.toString());
-
-//------------------------------------Events---------------------------------
-//			Optional<Event> ev1 = eventRepository.findEventById(1);
-//			ev1.ifPresent(value -> log.info(value.toString()));
-//
-//			Iterable<Event> ev2 = eventRepository.findEventsByTitle("JavaTasks");
-//			List<Event> ev2res = (List<Event>) ev2;
-//			log.info(ev2res.toString());
-//
-//			Iterable<Event> ev3 = eventRepository.findEventsByWorkspacesId(3);
-//			List<Event> ev3res = (List<Event>) ev3;
-//			log.info(ev3res.toString());
-
-//--------------------------------------Notes------------------------------
-//			Optional<Note> note1 = noteRepository.findNoteById(1);
-//			note1.ifPresent(value -> log.info(value.toString()));
-//
-//			Iterable<Note> note2 = noteRepository.findNotesByDescription("lab1");
-//			List<Note> note2res = (List<Note>) note2;
-//			log.info(note2res.toString());
-//
-//			Iterable<Note> note3 = noteRepository.findNotesByEventId(3);
-//			List<Note> note3res = (List<Note>) note3;
-//			log.info(note3res.toString());
-		};
-
-	}
-
+	// 	return new WebMvcConfigurer() {
+	// 		@Override
+	// 		public void addCorsMappings(CorsRegistry registry) {
+	// 			registry
+	// 				.addMapping("/entity-management/workspaces")
+	// 				.allowedOrigins("http://localhost:5173");
+	// 		}
+	// 	};
+	// }
 }
--- calendar/src/main/java/pet/project/calendar/web/NoteController.java
+++ calendar/src/main/java/pet/project/calendar/web/NoteController.java
@@ -17,6 +17,7 @@ import java.util.Map;
 import java.util.Objects;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/entity-management")
 public class NoteController {
 
